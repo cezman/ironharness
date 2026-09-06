a102c9967a06ac0dad730e2f1baeac45f02ddec5
cezman <balmaev@email.com>
2026-09-06 21:51:33 +0300

    feat: файловая песочница FileSandbox (границы, квоты, журнал) + errors.py

--- src/io_core/__init__.py
+++ src/io_core/__init__.py
@@ -1,5 +1,13 @@
 """io-core: безопасный I/O-слой для агентов (транспорты, журнал, политики, MCP)."""
 
+from io_core.errors import (
+    OperationTimeout,
+    QuotaExceeded,
+    RateLimitExceeded,
+    SandboxViolation,
+    VerificationError,
+)
+from io_core.file_sandbox import FileSandbox
 from io_core.journal import JsonlJournal, read_events
 from io_core.modbus_sim import ModbusSimServer
 from io_core.modbus_transport import ModbusTransport
@@ -9,11 +17,17 @@ from io_core.serial_transport import SerialTransport
 __version__ = "0.1.0"
 
 __all__ = [
+    "FileSandbox",
     "JsonlJournal",
     "ModbusSimServer",
     "ModbusTransport",
+    "OperationTimeout",
+    "QuotaExceeded",
+    "RateLimitExceeded",
     "ReplayMismatch",
     "ReplayTransport",
+    "SandboxViolation",
     "SerialTransport",
+    "VerificationError",
     "read_events",
 ]
