a8bb8843c88ea1208d0582ca70ba0c28dc967511
cezman <balmaev@email.com>
2026-09-06 21:27:23 +0300

    refactor: модуль firmbench переименован в ironbench (семья ironharness)

--- tests/test_smoke.py
+++ tests/test_smoke.py
@@ -1,11 +1,11 @@
-import firmbench
 import io_core
+import ironbench
 from io_core.mcp_server import echo
 
 
 def test_packages_importable():
     assert io_core.__version__
-    assert firmbench.__version__
+    assert ironbench.__version__
 
 
 def test_echo_tool():
